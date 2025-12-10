f17be300c719642fae43f8f3d2b760f0e3a8a81f
Stockholmalion <stockholmalion@gmail.com>
2025-12-10 21:47:28 +0000

    fix redirection1

--- routes/workouts.js
+++ routes/workouts.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 const redirectLogin = (req, res, next) => {
     if (!req.session.userId ) {
-      res.redirect('../auth/login') // redirect to the login page
+      res.redirect('workouts/../auth/login') // redirect to the login page
     } else { 
         next (); // move to the next middleware function
     } 
